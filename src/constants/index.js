dd3b54eea3b29aeedd5079aa11a8d8ae43bf7274
foreverkenyan <smuminaetx100@gmail.com>
2022-04-12 23:30:33 +0300

    Export constants

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import images from './images';
+import aboutsData from './aboutsData';
+import skillsData from './skillsData';
+
+export { images, aboutsData, skillsData };
---
+++ src/constants/skillsData.js
@@ -0,0 +1,7 @@
+import images from './images';
+
+const skillsData = [
+    {title:"Frontend development", description:"I am a good frontend developer with 2 years of experience", imageUrl: images.about01}
+]
+
+export default skillsData;
